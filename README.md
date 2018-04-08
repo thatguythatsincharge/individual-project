90022995f64f14801ab9b3826a331a88b49fa060
thatguythatsincharge <36935809+thatguythatsincharge@users.noreply.github.com>
2018-04-08 16:06:22 -0700

    still has issues with item page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# individual-project" 
+
+This is an ecomerce website i am building for my friends dad.
--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var mysql = require('mysql');
 var ejs = require('ejs');
+// connect to the db
+const con = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'aleman'
+});
 
 var app = express();
 
@@ -10,14 +17,6 @@ app.use(express.static('public'));
 app.use(bodyparser());
 
 app.get('/', function(req, res) {
-	// connect to the db
-	var con = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'aleman'
-	});
-	
 	// grab everthing from the database
 	var sql = "SELECT * FROM posts";
 
@@ -32,29 +31,28 @@ app.get('/', function(req, res) {
 
 app.get('/newPost', function(req, res) {
 	res.render('newPost', { activePage: 'newPost' });
-	
 });
 
 app.get('/Sign_in', function(req, res) {
 	res.render('sign_in', { activePage: 'signIn' });
 });
 
-app.get('/itemPage:id', function(req, res) {
+app.get('/itemPage/:id', function(req, res) {
 	var itemId = req.params.id;
-	if (err) {
-		throw err;
-	}
-	res.render('itemPage'+'/' + itemId);
+	var sql = "SELECT * FROM posts WHERE id = " + itemId;
+
+	con.query(sql, function(err, result) {
+		if (err) {
+			throw err;
+		}
+
+		res.render('itemPage', {
+		 currentItem: result[0], activePage: 'itemPage'
+		 });
+	});
 });
 
 app.post('/newlist', function(req, res) {
-	var con = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'root',
-		database: 'aleman'
-	});
-
 	var sql =
 		"INSERT INTO posts (item, description, price, image_url) VALUES ('" +
 		req.body.item +
@@ -72,14 +70,10 @@ app.post('/newlist', function(req, res) {
 		if (err) {
 			throw err;
 		}
-
 		console.log('1 record inserted');
 		res.redirect('/');
-		function confim() {
-			alert("Your post was submitted.");
-		};
+		});
 	});
-});
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('App is running at http://localhost:3000');
